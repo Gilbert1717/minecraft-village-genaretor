3d0fb335217c5105a01246df1f7988dcffedcd5b
gilbert <s3665887@student.rmit.edu.au>
2022-09-06 11:05:41 +1000

    Built house class,  door, window, frame, ground, roof methods

---
+++ House.py
@@ -0,0 +1,75 @@
+from mcpi.minecraft import Minecraft
+import random 
+class House:
+    def __init__ (self,x,y,z,stories,width,length):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.stories = stories
+        self.width = width
+        self.length = length
+
+    def frame(self,mc):
+        mc.setBlocks(self.x,self.y,self.z,self.x + self.width,self.y + 6,self.z + self.length,1)
+        mc.setBlocks(self.x + 1,self.y + 1,self.z + 1,self.x + self.width - 1,self.y + 5,self.z + self.length - 1,0)
+
+    # def wall(self,mc):
+
+    def door(self,mc):
+        x = random.randrange(int(self.width/2) - 1,self.width - 3)
+        door_height = 3
+        door_width = 2
+        mc.setBlocks(self.x + x, self.y + 1, self.z, self.x + x + door_width,self.y + door_height,self.z,0)
+
+    def window(self,mc):
+        window_x = self.x + 3
+        window_y = self.y + 3
+        window_height = window_x + 2
+        window_width = window_y + 2
+        mc.setBlocks(window_x, window_y, self.z, window_width,window_height,self.z,102)
+    
+    def back_window(self,mc):
+        window_x = self.x + 3
+        window_y = self.y + 1
+        window_z = self.z + self.length 
+        window_height = 5
+        window_width = window_x + 8
+        mc.setBlocks(window_x, window_y, window_z, window_width,window_height,window_z,102)
+    
+    def left_window(self,mc):
+        i = random.randint(1,3)
+        window_x = self.x + self.width
+        window_y = self.y + random.randint(1,3)
+        window_z = self.z + 3
+        window_height = window_y + 2
+        window_width = window_z + 1
+        for x in range(i):
+            print(window_z)
+            mc.setBlocks(window_x, window_y, window_z, window_x,window_height,window_width,102)
+            window_z = window_z + int(self.length/i) 
+            window_width = window_z + 1
+            
+    
+    def right_window(self,mc):
+        i = random.randint(1,3)
+        window_x = self.x
+        window_y = self.y + random.randint(1,3)
+        window_z = self.z + 3
+        window_height = window_y + 2
+        window_width = window_z + 1
+        for x in range(i):
+            print(window_z)
+            mc.setBlocks(window_x, window_y, window_z, window_x,window_height,window_width,102)
+            window_z = window_z + int(self.length/i)
+            window_width = window_z + 1
+            
+    def front(self,mc):
+        self.door()
+        self.window()
+
+    def ground(self,mc):
+        mc.setBlocks(self.x,self.y,self.z,self.x + self.width,self.y,self.z + self.length,5)
+
+    def roof(self,mc):
+        mc.setBlocks(self.x,self.y + 6 * self.stories,self.z,self.x + self.width,self.y + 6 * self.stories,self.z + self.length,2)
+
--- village.py
+++ village.py
@@ -1,8 +1,33 @@
 # Assignment 1 main file
 # Feel free to add additional modules/files as you see fit.
 
+from House import House
 from mcpi.minecraft import Minecraft
+from mcpi import block
+import random 
 
-mc = Minecraft.create()
-
-mc.postToChat("Hello world")
+if __name__ == "__main__":
+    mc = Minecraft.create()
+    mc.setBlocks(-200,0,-200,100,200,0)
+    mc.setBlocks(-200,-3,-200,0,200,2)
+    x = 0
+    y = 0
+    z = 0
+    # mc.player.setPos(x,y,z)
+    # mc.player.getPos()
+    # while mc.getBlock(x + 1,y,z + 1) is 0:
+    #     y = y - 1
+    # print(x,y,z)
+    # mc.setBlock(x + 1,y + 1,z + 1, 1)
+    # stories = random.randint(1,3)
+    # width = random.randrange(10,15)
+    # length = random.randrange(15,20)
+    house1 = House(x,y,z,1,16,20)
+    house1.frame(mc)
+    house1.ground(mc)
+    house1.roof(mc)
+    house1.door(mc)
+    house1.window(mc)
+    house1.back_window(mc)
+    house1.left_window(mc)
+    house1.right_window(mc)
