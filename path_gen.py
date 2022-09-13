8f0e1559599a190720bfb0e19e56d03002286476
Kurniawan Zhong Zhen <s3934639@student.rmit.edu.au>
2022-09-13 10:38:38 +1000

    used vec3 instead of (x,z) tuple in some codes to increase readability. reduced calls to getBlockHeight

--- path_gen.py
+++ path_gen.py
@@ -22,22 +22,27 @@ def get_random_coords(vil_start, vil_end, amount):
 
     for i in range(amount):
         too_close = True
-
+        
         while too_close:
             x = random.randint(vil_start.x, vil_end.x)
             z = random.randint(vil_start.z, vil_end.z)
-
             too_close = False
             
             for coord in coords:
                 # if math.fabs(coord[0] - x) + math.fabs(coord[1] - z) < min_distance_between_coords:
-                if math.sqrt((coord[0] - x) ** 2) + math.sqrt((coord[1] - z)** 2) < min_distance_between_coords:
+                
+                if math.sqrt((coord.x - x) ** 2) + math.sqrt((coord.z - z)** 2) < min_distance_between_coords:
                     too_close = True
                     break
             
-        coords.append((x, z))
-        mc.setBlock(x,getBlockHeight(x,z), z, 3)
-
+        coords.append(vec3.Vec3(x, 0, z))
+        
+        
+        mc.setBlock(x,
+                    100,
+                    z,
+                    3)
+    
     return coords
 
 def generate_plots(points, distance_dict):
@@ -50,30 +55,30 @@ def generate_plots(points, distance_dict):
 
 # https://en.wikipedia.org/wiki/Voronoi_diagram
 def generate_path_and_plots(vil_start, vil_end, vor_amount):
+    
     voronoi_points = get_random_coords(vil_start, vil_end, vor_amount)
     voronoi_distances = dict()
-    path_coords_set = set()
-
-
-
+    path_coords = []
 
 
     for x in range(vil_start.x, vil_end.x):
         for z in range(vil_start.z, vil_end.z):
 
-            
-            distances = []
+            distances = [] # list of distances from the current x,z iteration to all voronoi points
 
             for coord in voronoi_points:
-                distances.append([coord, math.fabs(coord[0] - x) + math.fabs(coord[1] - z)])
-                # distances.append([coord, math.sqrt((coord[0] - i) ** 2 + (coord[1] - j) ** 2)])
+                distances.append([(coord.x, coord.z), math.fabs(coord.x - x) + math.fabs(coord.z - z)])
             
-            distances.sort(key = lambda list : list[1])
+            distances.sort(key = lambda list : list[1]) # sorts distances in order of ascending distance
 
+            
+            distance_sub = distances[0][1] - distances[1][1] # difference in distance between the 2 closest vornoi points
 
-
-            distance_sub = distances[0][1] - distances[1][1]
             if distance_sub <= 1 and distance_sub >= -1:
+
+                path_coords.append(vec3.Vec3(x, 0, z)) # get the y coord later with getblockheight
+        
+                ############ this code block populates voronoi_distances with a voronoi point's distance to the closest path
                 if distances[0][0] not in voronoi_distances:
                     voronoi_distances[distances[0][0]] = distances[0][1]
 
@@ -84,20 +89,27 @@ def generate_path_and_plots(vil_start, vil_end, vor_amount):
 
                 elif voronoi_distances[distances[1][0]] > distances[1][1]:
                     voronoi_distances[distances[1][0]] = distances[1][1]
+                ###########
 
-                path_coords_set.update({(x-1,z+1),  (x,z+1),    (x+1,z+1),
-                                          (x-1,z),    (x,z),      (x+1,z),
-                                          (x-1,z-1),  (x,z-1),    (x+1,z-1)})
+                
                 #mc.setBlock(   x,    getBlockHeight(x, z),    z, 1)
-                #mc.setBlocks(   x-1,    getBlockHeight(x, z),    z-1, 
-                #                x+1,    getBlockHeight(x, z),    z+1, 1)
+                #mc.setBlocks(   x-1,    100,    z-1, 
+                                #x+1,    100,    z+1, 1)
     
-    for coord in path_coords_set:
-        mc.setBlock(coord[0], getBlockHeight(coord[0], coord[1]), coord[1] , 1)
-        
-    generate_plots(voronoi_points, voronoi_distances)
+    for coord in path_coords:
+        print('getting block height for', coord.x, coord.z)
+        coord.y = getBlockHeight(coord.x, coord.z)
 
-    return path_coords_set
+        mc.setBlock(coord.x, coord.y, coord.z, block.COBBLESTONE.id)
+    
+    for coord in path_coords:
+        print('placing blocks')
+        mc.setBlocks(   coord.x-1,    coord.y,    coord.z-1, 
+                        coord.x+1,    coord.y,    coord.z+1, block.COBBLESTONE.id)
+        
+    generate_plots(voronoi_points, voronoi_distances)   
+
+    return path_coords  
 
 
 def getBlockHeight(block_x, block_z):
@@ -127,5 +139,6 @@ if __name__ == '__main__':
     vil_end = vec3.Vec3(vil_start.x + vil_length, 
                         vil_start.y,
                         vil_start.z + vil_length)
-
+    
     generate_path_and_plots(vil_start, vil_end, num_points)
+   
