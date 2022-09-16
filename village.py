b77d473b79255f04b3fdcb1d42b48a1a46fd5d04
gilbert <s3665887@student.rmit.edu.au>
2022-09-16 12:05:12 +1000

    fix create sidewindow function

--- village.py
+++ village.py
@@ -16,34 +16,34 @@ from models.Structure import Structure, Vector
 import path_gen
 
 
-# def get_village_coords(mc):
-#     """checks for right clicks while holding a sword, returns the coordinate of the right clicked block in vec3"""
-#     blockevents = mc.events.pollBlockHits()
+def get_village_coords(mc):
+    """checks for right clicks while holding a sword, returns the coordinate of the right clicked block in vec3"""
+    blockevents = mc.events.pollBlockHits()
 
-#     mc.postToChat('right click on a block while holdling a sword to set village location')
-#     while blockevents == []:
-#         blockevents = mc.events.pollBlockHits()
+    mc.postToChat('right click on a block while holdling a sword to set village location')
+    while blockevents == []:
+        blockevents = mc.events.pollBlockHits()
     
-#     mc.postToChat('location set to ' + str(blockevents[0].pos))
-#     return blockevents[0].pos
+    mc.postToChat('location set to ' + str(blockevents[0].pos))
+    return blockevents[0].pos
 
 
 if __name__ == "__main__":
     mc = Minecraft.create()
-
-    # vil_length = 85
-    # num_points = 5
-
-    # vil_start = get_village_coords(mc)
-    # vil_end = vec3.Vec3(vil_start.x + vil_length, 
-    #                    vil_start.y,
-    #                    vil_start.z + vil_length)
-
-    # paths, plots = path_gen.generate_path_and_plots(vil_start, vil_end, num_points)
     
-    # for plot in plots:
-    #     structure = plot.get_structure()
-    #     plot.place_house(structure)
+    vil_length = 85
+    num_points = 5
+
+    vil_start = get_village_coords(mc)
+    vil_end = vec3.Vec3(vil_start.x + vil_length, 
+                       vil_start.y,
+                       vil_start.z + vil_length)
+
+    paths, plots = path_gen.generate_path_and_plots(vil_start, vil_end, num_points)
+    
+    for plot in plots:
+        structure = plot.get_structure()
+        plot.place_house(structure)
     
     
 
@@ -62,14 +62,14 @@ if __name__ == "__main__":
     # position = vec3.Vec3(0,0,0)
     # mc.setBlocks(x,y - 1,z,x + 30, y + 50, z + 30, 0)
     # mc.setBlocks(x + 10,y -1,z + 10,x - 30, y + 50, z - 30, 0)
-    position = mc.player.getTilePos()
-    weith = random.randint(8,12)
-    length = random.randint(12,16)
-    structure1 = Structure(position,weith,length)
-    print(structure1.frontleft.x,structure1.frontleft.z,structure1.frontright.x,structure1.frontright.z)
-    stories = random.randint(1,3)
-    house1 = House(structure1,3)
-    house1.create_house(mc)
+    # position = mc.player.getTilePos()
+    # weith = random.randint(8,12)
+    # length = random.randint(12,16)
+    # structure1 = Structure(position,weith,length)
+    # print(structure1.frontleft.x,structure1.frontleft.z,structure1.frontright.x,structure1.frontright.z)
+    # stories = random.randint(1,3)
+    # house1 = House(structure1,3)
+    # house1.create_house(mc)
     
     
     
