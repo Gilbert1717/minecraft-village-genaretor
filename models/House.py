2bae7a79d741cab6a0ffe76729918952bc81dd36
tommyStudies <s3845257@student.rmit.edu.au>
2022-09-18 21:59:10 +1000

    implemented lighting via the floor
    created a nested for loop, that iterates through the number of rows and columns
    
    of the floor and places a block to emit light in the room, which is determined
    
    by an offset that is generated randomly for x and z.

--- models/House.py
+++ models/House.py
@@ -21,10 +21,21 @@ class House:
     def create_floor(self, mc: Minecraft):
         material = rm.random_floors()
         colour = random.randint(1,3)
+        lightBlock_offset_z = random.randint(2, 4)
+        lightBlock_offset_x = random.randint(3, 4)
         for storey in range(self.stories):
             structure = self.structure
             floor = Floor(structure, storey)
             create_blocks(mc, floor.frontleft, floor.backright, material, colour)
+            
+            """ADDING LIGHTING BY INCORPORATING IT INTO THE FLOOR AND ITERATING THROUGH ROWS AND COLUMNS"""
+            for i in range(floor.frontleft.x, floor.backright.x, lightBlock_offset_x): 
+                block_difference_x = i - floor.frontleft.x # Setting the offset position to place glowstone on the row
+                for z in range(floor.frontleft.z, floor.backright.z, lightBlock_offset_z): 
+                    block_difference_z = z - floor.frontleft.z # Setting the offset position to place glowstone on column
+                    mc.setBlock(floor.frontleft.x + block_difference_x, floor.frontleft.y,
+                                floor.frontleft.z + block_difference_z, block.GLOWSTONE_BLOCK.id)
+                
             self.floors.append(floor)
 
     def create_rooms(self,mc):
