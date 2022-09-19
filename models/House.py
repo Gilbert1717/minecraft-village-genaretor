abe11f4f7276933577c7b9669ed57ebfb1633604
tommyStudies <s3845257@student.rmit.edu.au>
2022-09-19 21:19:49 +1000

    implemented create_furniture function
    goes through a nested loop to iterate the amount of times
    
    it needs to place furniture, and then places using the place_furniture
    
    function which takes room as an argument.

--- models/House.py
+++ models/House.py
@@ -28,7 +28,7 @@ class House:
             floor = Floor(structure, storey)
             create_blocks(mc, floor.frontleft, floor.backright, material, colour)
             
-            """ADDING LIGHTING BY INCORPORATING IT INTO THE FLOOR AND ITERATING THROUGH ROWS AND COLUMNS"""
+            """ADDING LIGHTING BY INCORPORATING IT INTO THE FLOOR BY ITERATING THROUGH ROWS AND COLUMNS"""
             for i in range(floor.frontleft.x, floor.backright.x, lightBlock_offset_x): 
                 block_difference_x = i - floor.frontleft.x # Setting the offset position to place glowstone on the row
                 for z in range(floor.frontleft.z, floor.backright.z, lightBlock_offset_z): 
@@ -169,8 +169,12 @@ class House:
         create_door(mc,self.structure.frontleft,self.structure.frontright)
         # self.front_window(mc)
 
-
-    
+    def create_furniture(self, mc):
+        
+        for floor in self.floors:
+            for room in floor.rooms:
+                floor.place_furniture(mc, room)
+                print("placing furniture")
 
     def create_house(self,mc):
         self.create_floor(mc)
@@ -179,6 +183,7 @@ class House:
         self.create_walls(mc)
         self.create_windows(mc)
         self.front_side(mc)
+        self.create_furniture(mc)
         # self.back_window(mc)
         # self.side_window(mc,self.structure.frontleft)
         # self.side_window(mc,self.structure.frontright)
